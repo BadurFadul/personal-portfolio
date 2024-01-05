33d9ac74be3a36fdcc1ae54f0701ed33bfbdcd7a
bad fad <badfad@edu.novia.fi>
2024-01-05 20:12:27 +0200

    nav and header setup

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,33 @@
+"use client"
 import React from 'react'
+
 import ThemeToggler from './ThemeToggler'
+import Logo from './Logo'
+import Nav from './Nav'
+import MobileNav from './MobileNav'
 
 const Header = () => {
   return (
-    <div>
-        <ThemeToggler />
-    </div>
+    <header>
+       <div className='container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <Logo />
+          <div className='flex items-center gap-x-6'>
+            {/* nav */}
+            <Nav
+              containerStyles='hidden xl:flex gap-x-8 items-center'
+              linkStyles='relative hover:text-primary transition-all'
+              underlineStyles='absolute left-0 top-full h-[2px] bg-primary w-full'
+            />
+            <ThemeToggler />
+            {/* mobile nav */}
+            <div className='xl:hidden'>
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
